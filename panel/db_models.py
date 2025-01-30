0be300647558b40dcaa9b19ff13a8b9d8be7d333
overklassniy <92175277+overklassniy@users.noreply.github.com>
2025-01-30 16:47:58 +0300

    spam_message table implemented to db.sqlite; detected spam pagination implemented

--- panel/db_models.py
+++ panel/db_models.py
@@ -8,3 +8,16 @@ class User(UserMixin, db.Model):
     name = db.Column(db.String(1000))
     password = db.Column(db.String(1000))
     can_configure = db.Column(db.Boolean, default=False)
+
+
+class SpamMessage(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.Float, nullable=False)
+    author_id = db.Column(db.BigInteger, nullable=False)
+    author_username = db.Column(db.String(255), nullable=False)
+    message_text = db.Column(db.Text, nullable=False)
+    has_reply_markup = db.Column(db.Boolean, default=False)
+    cas = db.Column(db.Boolean, default=False)
+    lols = db.Column(db.Boolean, default=False)
+    chatgpt_prediction = db.Column(db.Float, nullable=True)
+    bert_prediction = db.Column(db.Float, nullable=True)
--- panel/main.py
+++ panel/main.py
@@ -1,17 +1,17 @@
 import sys
 from datetime import datetime
 
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request
 from flask_login import login_required
 
 from panel.app import title, icon_path, background_path, root_css_path
+from panel.db_models import SpamMessage
 
 sys.path.append("../utils")
 from basic import load_json_file, config, plural_form
 
 main_bp = Blueprint('main', __name__)
 
-detected_spam_json_path = config['DETECTED_SPAM_JSON']
 
 # Пути к статическим ресурсам
 main_css_path = '/static/styles/main.css'
@@ -33,29 +33,47 @@ def index():
     username_text = 'Имя пользователя'
     spam_text = 'Текст сообщения'
     probability_text = 'Критерии'
+    prev_text = 'Предыдущая'
+    next_text = 'Следующая'
 
-    detected_spam_list = load_json_file(detected_spam_json_path, {})
-    count = len(detected_spam_list)
+    # Настройки пагинации
+    per_page = config['PER_PAGE']  # количество записей на странице
+    page = request.args.get('page', 1, type=int)  # получаем номер текущей страницы из запроса, по умолчанию 1
+
+    # Запрос с пагинацией
+    detected_spam_list = SpamMessage.query.order_by(SpamMessage.timestamp.desc()).paginate(page=page, per_page=per_page, error_out=False)
+
+    count = detected_spam_list.total
     rows = ""
-    for index, item in enumerate(detected_spam_list[::-1]):
-        date = datetime.fromtimestamp(item["timestamp"]).strftime("%Y-%m-%d %H:%M:%S")
+    for item in detected_spam_list.items:
+        date = datetime.fromtimestamp(item.timestamp).strftime("%d.%m.%Y\n%H:%M:%S")
         rows += f"""
-            <tr>
-                <td>{date}</td>
-                <td>{item['author_id']}</td>
-                <td>{item['author_username']}</td>
-                <td>{item['message_text'].replace('\n', '<br>')}</td>
-                <td class="probability">Имеет inline-клавиатуру: {item['has_reply_markup']}<br>Забанен CAS / LOLS: {max(item['cas'], item['lols'])}<br>Вердикт ChatGPT: {0 if not item['chatgpt_prediction'] or 500 else item['chatgpt_prediction']}<br>Вердикт RuBert: {item['bert_prediction'][0]}</td>
-            </tr>
-        """
-    detected_spam_text_table = f'Обнаруженный спам: {count} {plural_form(count, "запись", "записей", "записи")}'
+                <tr>
+                    <td>{date}</td>
+                    <td>{item.author_id}</td>
+                    <td>{item.author_username or 'N/A'}</td>
+                    <td class="spam_message">{item.message_text.replace('\n', '<br>')}</td>
+                    <td class="probability">
+                        Имеет inline-клавиатуру: {item.has_reply_markup}<br>
+                        Забанен CAS / LOLS: {max(item.cas, item.lols)}<br>
+                        Вердикт ChatGPT: {0 if not item.chatgpt_prediction else item.chatgpt_prediction}<br>
+                        Вердикт RuBert: {item.bert_prediction}
+                    </td>
+                </tr>
+            """
+
+    detected_spam_text_table = f'Обнаруженный спам: {count} {plural_form(count, "запись", "записей", "записи")} (показываются {per_page})'
+
+    # Добавим переменные для навигации по страницам
+    prev_url = url_for('main.index', page=detected_spam_list.prev_num) if detected_spam_list.has_prev else None
+    next_url = url_for('main.index', page=detected_spam_list.next_num) if detected_spam_list.has_next else None
+    total_pages = detected_spam_list.pages  # Общее количество страниц
+    current_page = detected_spam_list.page  # Текущая страница
 
     return render_template(
         'main.html',
-
         root_css_path=root_css_path,
         main_css_path=main_css_path,
-
         hamburger_js_path=hamburger_js_path,
 
         icon_path=icon_path,
@@ -74,8 +92,13 @@ def index():
         username_text=username_text,
         spam_text=spam_text,
         probability_text=probability_text,
-
-        rows=rows
+        rows=rows,
+        prev_url=prev_url,
+        prev_text=prev_text,
+        next_url=next_url,
+        next_text=next_text,
+        total_pages=total_pages,
+        current_page=current_page
     )
 
 
